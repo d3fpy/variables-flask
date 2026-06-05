6aa62f70f3f465c387894b04115b3344185ea097
x <136571437+d3fpy@users.noreply.github.com>
2026-06-05 19:50:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask, render_template
 
 @app.route('/test')
 def test():
-    local_name = 'test_name'
+    local_name = 'User'
     return render_template("test.html", name=local_name)
